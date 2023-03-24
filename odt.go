62e4855c53edcfcf894acd2ffff70f6ff64adb10
mark-summerfield <mark@qtrac.eu>
2023-03-24 09:05:34 +0000

    Open()

--- odt.go
+++ odt.go
@@ -7,6 +7,9 @@ import (
 	"archive/zip"
 	_ "embed"
 	"io"
+	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 //go:embed Version.dat
@@ -14,7 +17,7 @@ var Version string
 
 type Odt struct {
 	Filename string
-	Files    []File
+	Files    []*File
 }
 
 type File struct {
@@ -28,7 +31,7 @@ func Open(filename string) (*Odt, error) {
 		return nil, err
 	}
 	defer reader.Close()
-	odt := &Odt{Filename: filename, Files: make([]File, 0)}
+	odt := &Odt{Filename: filename, Files: make([]*File, 0)}
 	for _, file := range reader.File {
 		itemReader, err := file.Open()
 		if err != nil {
@@ -39,8 +42,16 @@ func Open(filename string) (*Odt, error) {
 		if err != nil {
 			return nil, err
 		}
-		odt.Files = append(odt.Files, File{Name: file.Name,
+		odt.Files = append(odt.Files, &File{Name: file.Name,
 			Text: string(raw)})
 	}
+	slices.SortFunc(odt.Files, func(a, b *File) bool {
+		au := strings.ToUpper(a.Name)
+		bu := strings.ToUpper(b.Name)
+		if au != bu {
+			return au < bu
+		}
+		return a.Name < b.Name
+	})
 	return odt, nil
 }
